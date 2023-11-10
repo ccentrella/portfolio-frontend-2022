1207802dc0ca440a3441a590b0f5083b2d1ceb56
Christopher Centrella <ccentrella.j@gmail.com>
2023-11-10 12:25:06 -0600

    change NextJS configuration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require('path');
+
+const nextConfig = {
+
+    experimental: {
+        appDir: true
+    },
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+    },
+    distDir: "dist/"
+}
 
 module.exports = nextConfig
