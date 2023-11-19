7e40d837d500cf35bdaf47878733833fa27806f9
Christopher Centrella <ccentrella.j@gmail.com>
2023-11-19 00:05:30 -0600

    update blog api methods

--- services/blog.js
+++ services/blog.js
@@ -1,23 +1,35 @@
 import {validateRecaptcha} from "@/services/recaptcha";
+import config from "@/config";
 
 export const createPost = async (post) => {
 
     const token = await validateRecaptcha();
-    return fetch('/api/v1/blog/', {
+    return fetch(`${config.NEXT_PUBLIC_BACKEND_URL}/api/v1/blog`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ post, 'g-recaptcha-response': token }),
     });
 }
 
+export const getPost = (slug) => {
+
+    return fetch(`${config.NEXT_PUBLIC_BACKEND_URL}/api/v1/blog/${slug}`)
+        .then((response) => {
+            return response.json()
+        });
+}
+
 export const getPosts = () => {
 
-    return fetch('/api/v1/blog/')
-        .then((response) => response.json());
+    return fetch(`${config.NEXT_PUBLIC_BACKEND_URL}/api/v1/blog`)
+        .then((response) => {
+            return response.json()
+        });
 }
 
 export const getLatestPost = () => {
 
-    return fetch("/api/v1/blog/latest_article")
-        .then((response) => response.json());
+    return fetch(`${config.NEXT_PUBLIC_BACKEND_URL}/api/v1/blog?limit=1`)
+        .then((response) => response.json())
+        .then((documents) => documents[0]);
 }
