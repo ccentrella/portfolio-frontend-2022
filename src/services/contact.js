ad4de20e84c83cc137336e585f63c8936d81cf5d
Christopher Centrella <ccentrella.j@gmail.com>
2023-11-23 15:13:29 -0600

    add src directory for source code

--- src/services/contact.js
+++ src/services/contact.js
@@ -3,7 +3,7 @@ import { validateRecaptcha } from "@/services/recaptcha";
 export const submitContactForm = async (contact) => {
 
     const token = await validateRecaptcha();
-    return fetch('/api/v1/contact/', {
+    return fetch(`${config.NEXT_PUBLIC_BACKEND_URL}/api/v1/contact/`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ contact, 'g-recaptcha-response': token }),
--- src/services/subscribe.js
+++ src/services/subscribe.js
@@ -3,7 +3,7 @@ import { validateRecaptcha} from "@/services/recaptcha";
 export const subscribe = async (subscriber) => {
 
     const token = await validateRecaptcha();
-    return fetch('/api/v1/subscribers/', {
+    return fetch(`${config.NEXT_PUBLIC_BACKEND_URL}/api/v1/subscribers/`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ subscriber, 'g-recaptcha-response': token }),
